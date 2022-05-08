05b5f563695e08836689aac1884ec9226e912018
Atageldi Didarov <didarov.atageldi@gmail.com>
2022-05-08 05:38:25 +0500

    clean up code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,41 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
 
-    <link rel="stylesheet" href="/css/main.css">
-    <livewire:styles>
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @livewireStyles
 </head>
 <body class="font-sans bg-gray-900 text-white">
-    <nav class="border-b border-gray-800">
-        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
-            <ul class="flex flex-col md:flex-row items-center">
-                <li>
-                    <a href="{{ route('movies.index') }}" class="uppercase">
-                        {{ config('app.name') }}
-                    </a>
-                </li>
-                <li class="md:ml-16 mt-3 md:mt-0">
-                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Filmler</a>
-                </li>
-                <li class="md:ml-6 mt-3 md:mt-0">
-                    <a href="{{ route('tv.index') }}" class="hover:text-gray-300">Teleýaýlymlar</a>
-                </li>
-                <li class="md:ml-6 mt-3 md:mt-0">
-                    <a href="{{ route('actors.index') }}" class="hover:text-gray-300">Aktýorlar</a>
-                </li>
-            </ul>
-            <div class="flex flex-col md:flex-row items-center">
-                <livewire:search-dropdown>
-            </div>
+<nav class="border-b border-gray-800">
+    <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+        <ul class="flex flex-col md:flex-row items-center">
+            <li class="font-semibold">
+                <a href="{{ route('movies.index') }}" class="uppercase">
+                    {{ config('app.name') }}
+                </a>
+            </li>
+        </ul>
+        <div class="flex flex-col md:flex-row items-center">
+            <livewire:search-dropdown>
         </div>
-    </nav>
-    @yield('content')
-    <footer class="border border-t border-gray-800">
-        <div class="container mx-auto text-sm px-4 py-6">
-            <b>Orazgylyjow Didar</b> tarapyndan ýasaldy
-        </div>
-    </footer>
-    <livewire:scripts>
-    @yield('scripts')
+    </div>
+</nav>
+@yield('content')
+<footer class="border border-t border-gray-800">
+    <div class="container mx-auto text-sm px-4 py-6">
+        <b>Orazgylyjow Didar</b> tarapyndan ýasaldy
+    </div>
+</footer>
+@livewireScripts
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
